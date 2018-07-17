50509623a36d4b4bc26aa5ea9531630b952e386d
Sakuralalala <32644480+Sakuralalala@users.noreply.github.com>
2018-07-17 20:42:27 +0800

    添加丢失部分

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        Debug.Log("开始游戏");
+
+        GameObject.Find("Canvas").transform.GetChild(0).gameObject.SetActive(false);
+        GameObject.Find("Canvas").transform.GetChild(1).gameObject.SetActive(true);
+    }
+    public void GameIntroduce()
+    {
+        Debug.Log("游戏介绍");
+        GameObject.Find("Canvas").transform.GetChild(0).gameObject.SetActive(false);
+        GameObject.Find("Canvas").transform.GetChild(3).gameObject.SetActive(true);
+    }
+    public void ExitGame()
+    {
+        Debug.Log("退出游戏");
+        Application.Quit();
+    }
+    public void ReturnStart()
+    {
+        GameObject.Find("Canvas").transform.GetChild(3).gameObject.SetActive(false);
+        GameObject.Find("Canvas").transform.GetChild(0).gameObject.SetActive(true);
+    }
+    public void NormalMode()
+    {
+        GameObject.Find("Canvas").transform.GetChild(1).gameObject.SetActive(false);
+        GameObject.Find("Canvas").transform.GetChild(2).gameObject.SetActive(true);
+    }
+    public void HardMode()
+    {
+        GameObject.Find("Canvas").transform.GetChild(1).gameObject.SetActive(false);
+        //进入hard模式
+    }
+    public void GameOne()
+    {
+        //第一章
+        SceneManager.LoadScene(1);
+    }
+    public void GameTwo()
+    {
+        //第二章
+        //SceneManager.LoadScene(2);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
