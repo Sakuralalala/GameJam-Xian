f8e198a78442ba5f2a7d8b5fefc0068d847037f7
Phantomhive123 <949805411@qq.com>
2018-07-17 09:52:36 +0800

    Level1&Level2

---
+++ Assets/Scripts/Usual/_Plane.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Plane : MonoBehaviour {
+
+    public _Line[] lines;
+
+    public Transform Check()
+    {
+        foreach(_Line line in lines)
+        {
+            if(line.GetState()!=Linestate.show)
+                return null;
+        }
+        return gameObject.transform;
+    }
+}
