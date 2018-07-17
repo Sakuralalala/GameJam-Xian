9974d53f863a6d8b6ea39f91679e711749b814c4
Phantomhive123 <949805411@qq.com>
2018-07-17 11:54:58 +0800

    Level3todo

--- Assets/Scripts/Usual/_Plane.cs
+++ Assets/Scripts/Usual/_Plane.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class _Plane : MonoBehaviour {
 
     public _Line[] lines;
+    public List<_Plane> children;
+
+    private void Start()
+    {
+        _Plane[] allPlanes=transform.parent.GetComponentsInChildren<_Plane>();
+        foreach(_Plane plane in allPlanes)
+        {
+            if (gameObject.name == plane.gameObject.name)
+                children.Add(plane);
+        }
+    }
 
     public Transform Check()
     {
